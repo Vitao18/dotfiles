b3d4bb7b09b49943ec2d2c53cbd13bb0ac80f401
Vitao18 <victoronline9@gmail.com>
2018-09-19 23:20:04 -0300

    keep track of my emacs config

---
+++ init.el
@@ -0,0 +1,46 @@
+;; Boring backup files
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+;; Enable use-packages
+(require 'use-package)
+
+;; Line Numbers
+(global-linum-mode t)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (wakatime-mode elm-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Wakatime
+(use-package wakatime-mode
+     :diminish wakatime-mode
+     :ensure t
+     :config       
+    (setq wakatime-api-key "70b484c2-74d0-4ab0-9510-cd425f08f7e3")
+    (setq wakatime-cli-path "~/usr/local/bin/wakatime")
+    (global-wakatime-mode))
